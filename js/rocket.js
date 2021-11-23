7385c6fd5256a691e629453e2cbf5672b9dc5327
Thxrujxn <94764099+Thxrujxn@users.noreply.github.com>
2021-11-22 21:05:08 -0500

    Made our rocket cool with drawings and etc.

--- js/rocket.js
+++ js/rocket.js
@@ -8,11 +8,26 @@ var changeState = function (state) {
 	document.getElementById('countdown').innerHTML = countdownNumber;
 
 	// countdown
-	if (state ==2) {
+	if (state == 2) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber-1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
 			
+			if (countdownNumber > 4 && countdownNumber<= 7) {
+				//be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else{
+				document.getElementById('nervous').className = 'nervous';
+			};
+
+			if (countdownNumber > 1 && countdownNumber<= 4) {
+				//can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+			
+
 			if (countdownNumber <= 0) {
 			changeState(3);
 			};
@@ -25,7 +40,7 @@ var changeState = function (state) {
 				console.log('randomNumber:',randomNumber)
 
 				// success
-				if (randomNumber > 5) {
+				if (randomNumber > 3) {
 					changeState(4);
 				} else {
 					changeState(5); // oh no!
